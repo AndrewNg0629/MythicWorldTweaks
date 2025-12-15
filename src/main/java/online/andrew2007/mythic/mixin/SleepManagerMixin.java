5ad6bd2d532d6458394568ab5b0287efb78aab09
Andrew <AndrewNg0629@outlook.com>
2025-12-15 20:07:15 +0800

    Fixed fake player sleep exclusion bug

--- src/main/java/online/andrew2007/mythic/mixin/SleepManagerMixin.java
+++ src/main/java/online/andrew2007/mythic/mixin/SleepManagerMixin.java
@@ -3,22 +3,21 @@ package online.andrew2007.mythic.mixin;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.SleepManager;
 import online.andrew2007.mythic.config.RuntimeController;
-import online.andrew2007.mythic.injected.interfaces.PlayerEntityMethodInjections;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Mixin(SleepManager.class)
 public class SleepManagerMixin {
-    @Inject(at = @At(value = "HEAD"), method = "update")
-    private void update(List<ServerPlayerEntity> players, CallbackInfoReturnable<Boolean> info) {
-        if (RuntimeController.getCurrentTParams().fakePlayerSleepExclusion()) {
-            players.removeIf(PlayerEntityMethodInjections::mythicWorldTweaks$isFake);
-        }
+    @ModifyVariable(at = @At(value = "HEAD"), method = "update", ordinal = 1, argsOnly = true)
+    private List<ServerPlayerEntity> removeFakePlayers(List<ServerPlayerEntity> players) {
+        ArrayList<ServerPlayerEntity> list = new ArrayList<>(players);
+        list.removeIf(ServerPlayerEntity::mythicWorldTweaks$isFake);
+        return list;
     }
 
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;isSleeping()Z"), method = "update")
